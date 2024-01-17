ce67b910d46516ec2ab6de09b094b74346738ae0
Ryan <rhyananiki@gmail.com>
2024-01-17 16:41:24 +0700

    Setting for Hosting

--- db.js
+++ db.js
@@ -1,12 +1,21 @@
 // db.js - Separation of Database Configuration
 const mysql = require('mysql2/promise');
 
+// dev ---
 const dbConfig = {
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'clinic-booking-app',
 };
+
+// prod ---
+// const dbConfig = {
+//   host: 'localhost',
+//   user: 'u1024490_cba',
+//   password: '+cbA24sukses',
+//   database: 'u1024490_clinic-booking-app',
+// };
 
 const pool = mysql.createPool(dbConfig);
 
